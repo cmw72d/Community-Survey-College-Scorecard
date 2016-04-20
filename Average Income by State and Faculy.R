367aa7125fa50005197c299a48c2595d7385f5e7
cmw72d <cmw72d@mail.missouri.edu>
2016-04-20 10:02:23 -0500

    Took out a line that repeated

--- Average Income by State and Faculy.R
+++ Average Income by State and Faculy.R
@@ -5,7 +5,7 @@ library(ggplot2)
 
 # this one works!!
 scorecard = group_by(Scorecard, st_fips) %>% filter(AVGFACSAL != "NA") %>% summarize(mean = mean(AVGFACSAL))
-survey = group_by(ss13pusa, ST) %>% filter(PINCP != "NA") %>% summarize(mean = mean(PINCP))
+
 # find average for state income in survey
 survey = group_by(ss13pusa, ST) %>% filter(PINCP != "NA") %>% summarize(mean = mean(PINCP))
 
